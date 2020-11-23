079d7ad276e2907907c4ae8ca09ea519608f3a1d
Caleb Grove <calebgrove41@gmail.com>
2020-11-22 19:00:04 -0500

    initial commit

---
+++ drills.js
@@ -0,0 +1,14 @@
+const BinarySearchTree = require('./binaryTree.js');
+
+var main = () => {
+  const BST = new BinarySearchTree();
+  BST.insert(3, 3);
+  BST.insert(1, 1);
+  BST.insert(4, 4);
+  BST.insert(6, 6);
+  BST.insert(9, 9);
+  BST.insert(2, 2);
+  BST.insert(5, 5);
+  BST.insert(7, 7);
+  return BST;
+};
